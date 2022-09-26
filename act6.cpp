f5583be4150a3d283e4ab69794b60ae80fbfec83
Nicolas Russo <nicolasrusso291@gmail.com>
2022-09-26 20:33:55 +0000

    hasta ej6 funcionando

--- act6.cpp
+++ act6.cpp
@@ -27,6 +27,22 @@ extern DigitalOut systemBlockedLed;
 
 extern pcSerialComMode_t pcSerialComMode;
 
+void newAvailableCommands()
+{
+    pcSerialComStringWrite( "Available commands:\r\n" );
+    pcSerialComStringWrite( "Press '1' to activate LED1\r\n" );
+    pcSerialComStringWrite( "Press '2' to activate LED2\r\n" );
+    pcSerialComStringWrite( "Press '3' to activate LED3\r\n" );
+    pcSerialComStringWrite( "Press '4' to show A/D value\r\n" );
+    pcSerialComStringWrite( "Press other key to deactivate LEDs\r\n" );
+    pcSerialComStringWrite( "\r\n" );
+}
+
+void newPcSerialComInit()
+{
+    newAvailableCommands();
+}
+
 void newUserInterfaceMatrixKeypadUpdate()
 {
     char keyReleased = matrixKeypadUpdate();
@@ -46,18 +62,24 @@ void ledInit(){
 
 void showAD(){
     char str[20]; 
-    sprintf(str, "%f\n", AD.read());
+    sprintf(str, "Potentiometer value: %f\n", AD.read());
     uartUsb.write(str, strlen(str));
 }
 
 void showButton(){
     char str[20]; 
-    sprintf(str, "Boton1: %d\n", Button1.read());
-    uartUsb.write(str, strlen(str));
-    sprintf(str, "Boton2: %d\n", Button2.read());
-    uartUsb.write(str, strlen(str));
-    sprintf(str, "Boton3: %d\n", Button3.read());
-    uartUsb.write(str, strlen(str));
-    sprintf(str, "Boton4: %d\n", Button4.read());
-    uartUsb.write(str, strlen(str));
+    int buttonPressed = 0;
+    if (Button1.read() == ON){
+        buttonPressed = 1;
+    } else if (Button2.read() == ON){
+        buttonPressed = 2;
+    } else if (Button3.read() == ON){
+        buttonPressed = 3;
+    } else if (Button4.read() == ON){
+        buttonPressed = 4;
+    }
+    if (buttonPressed){
+        sprintf(str, "Button %d pressed\n", buttonPressed );
+        uartUsb.write(str, strlen(str));
+    }
 }
